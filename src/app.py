ed93a10104ba8b3229751974f69c118a87e32a5c
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2023-08-18 12:31:08 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ def spending():
     return render_template('Shukkin_Form.html')
 
 #データベース接続
-@app.route('/in/<int:page>', methods = ['POST'])
+@app.route('/in_<int:page>', methods = ['POST'])
 def connectDB(page):
     with open('secret.json') as f:
         dbInfo = json.load(f)
@@ -29,7 +29,7 @@ def connectDB(page):
         passwd = dbInfo['pass'],
         db = dbInfo['db'],
         use_unicode = True,
-        charset = "utf8"
+        charset = 'utf8'
     )
     cursor = connect.cursor(MySQLdb.cursors.DictCursor)
 
@@ -39,33 +39,39 @@ def connectDB(page):
         budget_money = request.form.get('money')
         budget = int(budget_money)
         today = datetime.date.today()
+        flag = False
 
-        cursor.execute('SELECT * FROM Yosan WHERE user_id = %s', (budget_userID))
+        cursor.execute('SELECT * FROM Yosan')
         rows = cursor.fetchall()
 
-        if rows == []:
+        for row in rows:
+            if row['user_id'] == budget_userID:
+                flag = True
+
+        if flag:
+            cursor.execute('UPDATE Users SET user_name = %s WHERE user_id = %s', (userName, budget_userID))
+            cursor.execute('UPDATE Yosan SET zandaka = %s, torokubi = %s WHERE user_id = %s', (budget, today, budget_userID))
+        else:
             cursor.execute('INSERT INTO Users VALUES(%s, %s)', (budget_userID, userName))
             cursor.execute('INSERT INTO Yosan VALUES(%s, %s, %s)', (budget_userID, budget, today))
-        else:
-            cursor.execute('UPDATE Users SET user_name = %s', (userName))
-            cursor.execute('UPDATE Yosan SET zandaka = %s, torokubi = %s WHERE user_id = %s', (budget, today, budget_userID))
 
         connect.commit()
+        cursor.close()
         connect.close()
 
         return render_template('Yosan_Complete.html')
     elif page == 2: #支出入力
         spending_userID = request.form.get('spending_userID')
         categoryNumber = request.form.get('category')
-        if categoryNumber == 1:
+        if categoryNumber == '1':
             category = '食費'
-        elif categoryNumber == 2:
+        elif categoryNumber == '2':
             category = '交通費'
-        elif categoryNumber == 3:
+        elif categoryNumber == '3':
             category = '日用品費'
-        elif categoryNumber == 4:
+        elif categoryNumber == '4':
             category = '被服費'
-        elif categoryNumber == 5:
+        elif categoryNumber == '5':
             category = '娯楽費'
         spending_money = request.form.get('money')
         spending = int(spending_money)
@@ -76,5 +82,5 @@ def connectDB(page):
         connect.close()
         return render_template('Shukkin_Complete.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
