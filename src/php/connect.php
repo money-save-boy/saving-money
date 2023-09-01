514c4ee15fc1cf43b24bf152ae50914f04352a3b
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-09-01 14:30:29 +0900

    修正中

--- src/php/connect.php
+++ src/php/connect.php
@@ -4,30 +4,4 @@
     const USER = 'LAA1516911';
     const PASS = 'Pass0120';
     $connect = 'mysql:host='. SERVER. ';dbname='. DBNAME. ';charset=utf8';
-
-    //header('Access-Control-Allow-Origin: *');
-    //header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
-    //header("Content-type: application/json; charset=UTF-8");
-
-    $input_json = file_get_contents('php://input');
-    $post = json_decode( $input_json, true );
-    $ID = $post['sub'];
-    // データベースへの接続を確立
-    try {
-        $pdo = new PDO($connect, USER, PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "データベースへの接続に失敗しました: " . $e->getMessage();
-        exit;
-    }
-    try {
-        $query = "SELECT * FROM Users WHERE user_id = :id"; // your_table にテーブル名を入れてください
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':id', $ID);
-        $stmt->execute();
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        
-    } catch (PDOException $e) {
-        echo "クエリの実行に失敗しました: " . $e->getMessage();
-    }
 ?>
