196a9d138225b2e762817e810001ef7a8a23a537
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2023-09-03 08:02:01 +0900

    Create History_year.php

---
+++ src/templates/php/History_year.php
@@ -0,0 +1,17 @@
+<?php
+    $pdo = new PDO($connect, USER, PASS);
+    $str = 'select DATE_FORMAT(torokubi, "%Y") as torokubi, category, sum(money) from History where user_id = ? group by category';
+    $sql = $pdo -> query($str);
+    $sql -> execute($ID);
+    if(is_array($sql)){
+        foreach($sql as $row){
+            echo '<div class="data">';
+            echo '<p>', $row['torokubi'], '</p>';
+            echo '<p>', $row['category'], '</p>';
+            echo '<p>', $row['sum(money)'], '</p>';
+            echo '</div>';
+        }
+    }else{
+        echo '<h2>Your data does not exist</h2>';
+    }
+?>
