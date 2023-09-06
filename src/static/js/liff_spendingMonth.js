6e6ad0a2b8af197f5f83d95cc5a83a69568988cf
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-09-06 14:09:54 +0900

    修正版　１

--- src/static/js/liff_spendingMonth.js
+++ src/static/js/liff_spendingMonth.js
@@ -2,20 +2,19 @@ window.onload = function () {
     fetch("/src/secret.json")
         .then((response) => response.json())
         .then((data) => {
+            clearToken(data.spendingMonthLiffID);
             liff.init({
-                liffId: data.liffID,
+                liffId: data.spendingMonthLiffID,
                 withLoginOnExternalBrowser: true
             })
             .then(() => {
-                if(!liff.isLoggedIn()){
-                    liff.login();
-                }
+
                 var idToken = liff.getIDToken();
-                var postData = "id_token=" + idToken + "&client_id=" + data.channelID;
+                var postData = "id_token=" + idToken + "&client_id=" + data.channelID;            
                 fetch("https://api.line.me/oauth2/v2.1/verify", {
                     method: "POST",
                     headers: {
-                        "Content-Type": "application/x-www-form-urlencoded"
+                        "Content-Type": "application/x-www-form-urlencoded" //送られたトークンが本当にLINEから来たのか
                     },
                     body: postData
                 })
@@ -23,7 +22,7 @@ window.onload = function () {
                 .then((liffData) => {
                     var id = liffData.sub;
                     // JSONデータをPHPに送信
-                    fetch("/src/php/GetID.php", {
+                    fetch("https://liff.line.me/2000397164-26b145gY", {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json"
@@ -41,4 +40,35 @@ window.onload = function () {
                 });
             });
         });
+}
+
+function keyReload(el){
+    var keys = [];
+    for(var i = 0; i < localStorage.length; i++){
+        var key = localStorage.key(i);
+        if(key.indexOf(el) == 0){
+            keys.push(key);
+        }
+    }
+
+    return keys;
+}
+
+function clearToken(id){
+    var keyPrefix = `LIFF_STORE:${id}:`;
+    var key = keyPrefix + 'decodedIDToken';
+    var deIDTokenString = localStorage.getItem(key);
+
+    if(!deIDTokenString){
+        return;
+    }
+
+    var deIDToken = JSON.parse(deIDTokenString);
+
+    if (new Date().getTime() > deIDToken.exp * 1000){
+        var keys = keyReload(keyPrefix);
+        keys.forEach(function(key) {
+            localStorage.removeItem(key)
+        })
+    }
 }
--- src/templates/php/Savemoney_m.php
+++ src/templates/php/Savemoney_m.php
@@ -7,7 +7,33 @@
     <script src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="/src/static/js/postID.js"></script>
     <?php include('connect.php'); ?>
-    <?php include('GetID.php'); ?>
+    <?php
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+        echo "リクエストメソッド: " . $requestMethod;
+        // リクエストがPOSTメソッドでない場合はエラーを返す
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405); // メソッド不許可のステータスコード
+            die('Method Not Allowed');
+        }
+
+        // JSONデータを受け取り、連想配列に変換する
+        $jsonData = file_get_contents('php://input');
+        $data = json_decode($jsonData, true);
+
+        // JSONデータの受信と変換が成功したかを確認する
+        if ($data === null) {
+            http_response_code(400); // バッドリクエストのステータスコード
+            die('Invalid JSON data');
+        }
+
+        // 受け取ったIDを変数として定義
+        if (isset($data['id'])) {
+            $ID = $data['id'];
+        } else {
+            http_response_code(400); // バッドリクエストのステータスコード
+            die('Missing "id" field in JSON data');
+        }
+    ?>
     <title>支出額表示</title>
 </head>
 <body>
