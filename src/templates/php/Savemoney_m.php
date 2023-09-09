989f86cf3c709ee4482cbd369f72a23b390eb4a2
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-09-09 12:05:38 +0900

    Update Savemoney_m.php

--- src/templates/php/Savemoney_m.php
+++ src/templates/php/Savemoney_m.php
@@ -3,11 +3,15 @@
 <html>
 <head>
     <meta lang="ja">
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="/src/static/css/style.css">
     <script src="https://kit.fontawesome.com/fd4cebc555.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js"></script>
     <script src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="/src/static/js/liff_spendingMonth.js"></script>
+    <title>支出額表示</title>
+</head>
+<body>
     <?php include('connect.php'); ?>
     <?php
     $requestMethod = $_SERVER['REQUEST_METHOD'];
@@ -36,9 +40,6 @@
             echo 'こんにちは';
         }
     ?>
-    <title>支出額表示</title>
-</head>
-<body>
     <div class="osirase">
         <h1>お知らせ</h1>
         <input type="button" value="ログアウト" id="logout">
