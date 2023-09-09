94c9ac65dbeae900d70bf244022662aad5d60091
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-09-09 14:12:16 +0900

    Update History_m.php

--- src/templates/php/History_m.php
+++ src/templates/php/History_m.php
@@ -1,5 +1,10 @@
 <?php
-    echo $ID;
+    if(isset($ID)){
+        echo $ID;
+    }else{
+        echo 'IDもらえてないよ';
+    }
+    
     $pdo = new PDO($connect, USER, PASS);
     $str = 'select DATE_FORMAT(torokubi, "%Y-%m") as torokubi, sum(tyokin) from Tyokin where user_id = ? group by torokubi';
     $sql = $pdo -> query($str);
