d22c51f46dbf5ae1c828fdb7eeb08c7fa0787eb0
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-09-11 11:13:53 +0900

    Update History_month.php

--- src/templates/php/History_month.php
+++ src/templates/php/History_month.php
@@ -1,23 +1,4 @@
 <?php
-    /*$pdo = new PDO($connect, USER, PASS);
-    $str = 'select DATE_FORMAT(torokubi, "%Y-%m") as torokubi, category, sum(money) from History where user_id = ? group by torokubi,category';
-    $sql = $pdo -> query($str);
-    if(isset($ID)){
-        $sql -> execute($ID);
-    }else{
-        echo '<h2>Your ID does not exist</h2>';
-    }
-    if(is_array($sql)){
-        foreach($sql as $row){
-            
-            
-        }
-    }else{
-        
-    }*/
-?>
-
-<?php
 try {
     // データベース接続情報
     $pdo = new PDO(
