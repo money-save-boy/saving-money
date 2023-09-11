d75ec27f58234a03ae90d3651f55f506d25f4b53
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-09-11 09:49:21 +0900

    Update Tyokin_hyoji.php

--- src/templates/php/Tyokin_hyoji.php
+++ src/templates/php/Tyokin_hyoji.php
@@ -2,7 +2,7 @@
     $pdo = new PDO($connect, USER, PASS);
     $str = 'select tyokin from Tyokin where user_id = ?';
     $sql = $pdo->prepare($str);
-    $sql->execute([$ID]); //$IDをバインドした書き方
+$sql->execute(/*[$ID]*/["1"]); //$IDをバインドした書き方
     $result = $sql->fetch(PDO::FETCH_ASSOC); //取得したデータを連想配列として取得する
 
     if ($result !== false) {
