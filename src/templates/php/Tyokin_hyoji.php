1da3194904610f55456b7486e8edac5330ceb853
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-09-11 09:43:00 +0900

    Update Tyokin_hyoji.php

--- src/templates/php/Tyokin_hyoji.php
+++ src/templates/php/Tyokin_hyoji.php
@@ -1,4 +1,4 @@
-<?php
+/* <?php
     $pdo = new PDO($connect, USER, PASS);
     $str = 'select tyokin from Tyokin where user_id = ?';
     $sql = $pdo -> query($str);
@@ -14,5 +14,25 @@
                 echo '<p id="minus">貯金額 ¥ -', $row['tyokin'], '</p>';
             }
         }
+    }
+?> */
+
+<?php
+    $pdo = new PDO($connect, USER, PASS);
+    $str = 'select tyokin from Tyokin where user_id = ?';
+    $sql = $pdo->prepare($str);
+    $sql->execute([$ID]); //$IDをバインドした書き方
+    $result = $sql->fetch(PDO::FETCH_ASSOC); //取得したデータを連想配列として取得する
+
+    if ($result !== false) {
+        $tyokin = $result['tyokin'];
+        if ($tyokin > 0) {
+            echo '<p id="mod">貯金額 ¥ ', $tyokin, '</p>';
+        } else {
+            echo '<p id="minus">貯金額 ¥ -', $tyokin, '</p>';
+        }
+    } else {
+        echo '<p id="mod">貯金額 ¥ 0</p>';
+        echo $result;
     }
 ?>
