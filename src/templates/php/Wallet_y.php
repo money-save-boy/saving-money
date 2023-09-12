a2eedaf3bdfdb4a568fbf3a531acaa94abde9d4b
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-09-12 22:14:43 +0900

    update

--- src/templates/php/Wallet_y.php
+++ src/templates/php/Wallet_y.php
@@ -30,7 +30,7 @@
     </div>
     <footer>
         <div class="PageChange1" onclick="location.href='/src/spending_month'">
-            <p id="pig" class="fa-solid fa-piggy-bank" style="color: #ffffff; font-size: 3em; filter: drop-shadow(0 5px 5px #7e3459); margin-top:12%;"></p>
+            <p id="pig" class="fa-solid fa-piggy-bank" style="color: #ffffff; font-size: 3em; filter: drop-shadow(0 5px 5px #7e3459); margin-top:12.9%;"></p>
         </div>
         <div class="PageChange2">
             <p id="pig" class="fa-solid fa-wallet" style="color: #ffffff; font-size: 3em; filter: drop-shadow(0 5px 5px #7a1d34); margin-top:13%;"></p>
