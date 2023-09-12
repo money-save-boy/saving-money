0428c3398b63183d27efd92979ba83b57510058a
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2023-09-12 18:15:38 +0900

    Update liff_spendingMonth.js

--- src/static/js/liff_spendingMonth.js
+++ src/static/js/liff_spendingMonth.js
@@ -77,14 +77,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 })
                 .then((displaySpending) => displaySpending.json())
                 .then((spending) => {
-                    console.log(spending);
                     var div = document.getElementsByClassName("History")[0];
                     var text = "<div id='histitle'>支出履歴</div>";
                     for(var i = 0; i < spending[0].length; i++){
                         text += "<div class='data'>";
                         text += "<p>" + spending[0][i] + "</p>";
                         text += "<p>" + spending[1][i] + "</p>";
-                        text += "<p>" + spending[2][i] + "</p>";
+                        text += "<p>" + String(spending[2][i]).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,') + "</p>";
                         text += "</div>";
                     }
                     div.innerHTML = text;
