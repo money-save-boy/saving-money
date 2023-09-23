6f12582e64b2c4e4df95182085074c301dae9bea
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2023-09-24 00:06:44 +0900

    更新

--- src/templates/php/Tyokin_year_graph.php
+++ src/templates/php/Tyokin_year_graph.php
@@ -1,43 +1,30 @@
 <?php
-    try {
-            $pdo = new PDO(
-                $connect, USER, PASS,
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::ATTR_EMULATE_PREPARES =>false
-                ]
-            );
-            $sql = "SELECT SUM(tyokin),DATE_FORMAT(torokubi, '%Y') as yea FROM Tyokin
-                    WHERE user_id = ?
-                    GROUP BY yea";
-            $stmt = $pdo->query($sql);
-            $stmt -> execute(/*[$ID]*/['1']);
-            $result = $stmt->fetchAll();
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
+try {
+    $currentYear = date("Y");
+    $years = array();
+    for ($i = 0; $i < 5; $i++) {
+        $year = $currentYear - $i;
+        $years[] = $year;
+    }
+} catch (Throwable $e) {
+    echo $e->getMessage();
+}
 ?>
 <script>
-    var ctx = document.getElementById('myChart').getContext('2d');//2D画像として描画
-    <?php if(!is_array($result)){
-        echo '<h2>Your data does not exist</h2>';
-    }?>
+    var ctx = document.getElementById('myChart').getContext('2d'); //2D画像として描画
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: [<?php foreach($result as $row){
-                        echo '"'.($row["yea"]).'",';
-             }?>],
+            labels: [<?php for ($i = 4; $i >= 0; $i--) {
+                            echo '"' . ($years[$i]) . '",';
+                        } ?>],
             datasets: [{
-                label: '支出額',
-                data: [<?php foreach($result as $row){
-                    echo $row["SUM(tyokin)"].",";
-                } ?>],
-                backgroundColor: 
-                    'rgba(2, 164, 135, 0.2)',
-                borderColor: 
-                    'rgba(2, 164, 135, 1)',
+                label: "貯金額",
+                data: [<?php for ($i = 0; $i < 5; $i++) {
+                            echo '0,';
+                        } ?>],
+                backgroundColor: 'rgba(2, 164, 135, 0.2)',
+                borderColor: 'rgba(2, 164, 135, 1)',
                 borderWidth: 1
             }]
         },
@@ -49,9 +36,9 @@
                     }
                 }],
                 yAxes: [{
-                    ticks: {     
+                    ticks: {
                         fontSize: 10, // 数字のフォントサイズを設定
-                        beginAtZero: true,//グラフの初期値を０に指定
+                        beginAtZero: true, //グラフの初期値を０に指定
                     }
                 }]
             }
